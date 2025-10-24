05ae54fef2e20d45d85180e78981abf7f08680f7
abubakar <abubakarafzal171@gmail.com>
2025-10-25 01:17:50 +0500

    navbar ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './Pages/Home'
+import About from './Pages/About' 
+import Html from './Pages/Html'
 const App = () => {
   return (
-    <div>App</div>
+<>
+
+<Navbar/>
+<Routes>
+<Route path='/' element={<Home/>}></Route>
+<Route path='/about' element={<About/>}></Route>
+<Route path='/html' element={<Html/>}></Route>
+</Routes>
+
+
+</>
   )
 }
 
